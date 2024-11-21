a15e9fd361eadc84b3b71a75a09bd752700f3233
Alex Venegas <alfredo.alexis30@gmail.com>
2024-11-21 08:57:38 -0600

    Update to yarn bugs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,8 @@ import AppRoutes from "./routes/Routes.jsx";
 import "./css/index.css";
 import "./css/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "primereact/resources/themes/nano/theme.css"
 import "bootstrap-icons/font/bootstrap-icons.css";
-import 'primereact/resources/themes/lara-light-indigo/theme.css'; 
 import 'primereact/resources/primereact.min.css'; 
 import { LoaderProvider } from "./context/Loader/LoaderProvider.jsx";
 
